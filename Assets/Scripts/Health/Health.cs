55724daf9ec80ba981ed0cb1086f5f556ba83383
Josenn <josenn44@gmail.com>
2020-05-26 13:53:58 -0500

    Added health and damage system

---
+++ Assets/Scripts/Health/Health.cs
@@ -0,0 +1,80 @@
+﻿#region CLASS DESCRIPTION
+/******************************************************************
+**                                                               **
+**              Jonathan Renn                                    **
+**                                                               **
+**              Copyright (C) 2020 - All Rights Reserved         **
+**                                                               **
+*******************************************************************
+**                                                               **
+**      Project:             Glitch Garden                       **
+**                                                               **
+**      Module:              Health                              **
+**                                                               **
+**      File name:           Health.cs                           **
+**                                                               **
+**      Created by:          05/24/20            - JJR           **
+**                                                               **
+**      Description:         Controls a character's health       **
+**                                                               **
+**                                                               **
+**                                                               **
+**                                                               **
+**                                                               **
+**                                                               **
+**                                                               **
+**                                                               **
+******************************************************************/
+
+// This isn't actually under any copyright but I want to get in the habit of always including this
+
+#endregion // CLASS DESCRIPTION
+
+#region USING DIRECTIVES
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#endregion // USING DIRECTIVES
+
+#region HEALTH CLASS DEFINITION
+public class Health : MonoBehaviour
+{
+	 #region PRIVATE VARIABLES
+
+	[SerializeField] private float health;
+    [SerializeField] private ParticleSystem deathFX;
+
+	#endregion // PRIVATE VARIABLES
+
+	#region UNITY FUNCTIONS
+    void Start()
+    {
+        health = 100.0f;
+    }
+
+    void Update()
+    {
+		//Debug.Log("Firing!");
+    }
+
+	#endregion // UNITY FUNCTIONS
+
+    #region PUBLIC FUNCTIONS
+
+    public void DealDamage(float damage)
+    {
+        health -= damage;
+
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    #endregion // PUBLIC FUNCTIONS
+
+} // Class Health
+
+#endregion // CLASS DEFINITION
