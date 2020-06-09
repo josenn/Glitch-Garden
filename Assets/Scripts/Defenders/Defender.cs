6f0cb57759133daf2830d5bad8eba38c5c9da768
Josenn <josenn44@gmail.com>
2020-06-09 13:20:31 -0500

    Created more defender classes (see class description in each file)

---
+++ Assets/Scripts/Defenders/Defender.cs
@@ -0,0 +1,84 @@
+﻿#region CLASS DESCRIPTION
+
+/******************************************************************
+**                                                               **
+**              Jonathan Renn                                    **
+**                                                               **
+**              Copyright (C) 2020 - All Rights Reserved         **
+**                                                               **
+*******************************************************************
+**                                                               **
+**      Project:             Glitch Garden                       **
+**                                                               **
+**      Module:              Defender                            **
+**                                                               **
+**      File name:           Defender.cs                         **
+**                                                               **
+**      Created by:          06/09/20            - JJR           **
+**                                                               **
+**      Description:         A template file                     **
+**                                                               **
+**      This file is to be used as a template for other files    **
+**      Just change the class name                               **
+**                                                               **
+**                                                               **
+**                                                               **
+**                                                               **
+******************************************************************/
+
+#endregion // CLASS DEFINITION
+
+
+#region USING DIRECTIVES
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+#endregion // USING DIRECTIVES
+
+
+#region DEFENDER CLASS DEFINITION
+public class Defender : MonoBehaviour
+{
+    #region PUBLIC VARIABLES
+
+    #endregion // PUBLIC VARIABLES
+
+	#region PRIVATE VARIABLES
+
+    [Tooltip("The amount of stars a defender cost to use")]
+    [SerializeField] int starCost = 100;
+
+
+    #endregion // PRIVATE VARIABLES
+
+    #region UNITY FUNCTIONS
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+
+    }
+
+    #endregion // UNITY FUNCTIONS
+
+    #region PUBLIC FUNCTIONS
+
+    #endregion // PUBLIC FUNCTIONS
+
+	#region PRIVATE FUNCTIONS
+
+	#endregion // PRIVATE FUNCTIONS
+
+	#region TODOS
+
+	#endregion // TODOS
+
+} // Class Defender
+
+#endregion // CLASS DEFINITION
---
+++ Assets/Scripts/Defenders/DefenderButton.cs
@@ -0,0 +1,102 @@
+﻿#region CLASS DESCRIPTION
+
+/******************************************************************
+**                                                               **
+**              Jonathan Renn                                    **
+**                                                               **
+**              Copyright (C) 2020 - All Rights Reserved         **
+**                                                               **
+*******************************************************************
+**                                                               **
+**      Project:             Glitch Garden                       **
+**                                                               **
+**      Module:              Defenders                           **
+**                                                               **
+**      File name:           DefenderButton.cs                   **
+**                                                               **
+**      Created by:          06/08/20            - JJR           **
+**                                                               **
+**      Description:         Controls buttons for defenders      **
+**                                                               **
+**      This class controls functionality for the defender       **
+**      selection buttons in the game                            **
+**                                                               **
+**                                                               **
+**                                                               **
+**                                                               **
+******************************************************************/
+
+#endregion // CLASS DEFINITION
+
+
+#region USING DIRECTIVES
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+#endregion // USING DIRECTIVES
+
+
+#region DEFENDER BUTTON CLASS DEFINITION
+public class DefenderButton : MonoBehaviour
+{
+    #region PUBLIC VARIABLES
+
+    #endregion // PUBLIC VARIABLES
+
+	#region PRIVATE VARIABLES
+
+    private Color buttonColor;
+
+    [SerializeField] Defender defenderPrefab;
+
+
+    #endregion // PRIVATE VARIABLES
+
+    #region UNITY FUNCTIONS
+
+    private void Start()
+    {
+        buttonColor = Color.white;
+    }
+
+    private void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// OnMouseDown is called when the user has pressed the mouse button while
+    /// over the GUIElement or Collider.
+    /// </summary>
+    private void OnMouseDown()
+    {
+        var buttons = FindObjectsOfType<DefenderButton>(); // Look for all of the defender buttons
+
+        foreach(DefenderButton button in buttons)
+        {
+            button.GetComponent<Image>().color = new Color32(43, 43, 43, 255);
+        }
+        //Debug.Log("You have selected a defender!!");
+        gameObject.GetComponent<Image>().color = buttonColor;
+    }
+
+    #endregion // UNITY FUNCTIONS
+
+    #region PUBLIC FUNCTIONS
+
+    #endregion // PUBLIC FUNCTIONS
+
+	#region PRIVATE FUNCTIONS
+
+	#endregion // PRIVATE FUNCTIONS
+
+	#region TODOS
+
+	#endregion // TODOS
+
+} // Class DefenderButton
+
+#endregion // CLASS DEFINITION
