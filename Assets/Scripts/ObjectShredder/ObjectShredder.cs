276852354f6661a9531b2da94c5692a7ed2375eb
Josenn <josenn44@gmail.com>
2020-06-17 19:01:33 -0500

    Added shredder for excess objects

---
+++ Assets/Scripts/ObjectShredder/ObjectShredder.cs
@@ -0,0 +1,54 @@
+﻿#region CLASS DESCRIPTION
+
+/******************************************************************
+**                                                               **
+**              Jonathan Renn                                    **
+**                                                               **
+**              Copyright (C) 2020 - All Rights Reserved         **
+**                                                               **
+*******************************************************************
+**                                                               **
+**      Project:             Glitch Garden                       **
+**                                                               **
+**      Module:              ObjectShredder                      **
+**                                                               **
+**      File name:           ObjectShredder.cs                   **
+**                                                               **
+**      Created by:          06/17/20            - JJR           **
+**                                                               **
+**      Description:         Destroys excess game objects        **
+**                           once they have left the play area   **
+**                                                               **
+**                                                               **
+**                                                               **
+**                                                               **
+**                                                               **
+**                                                               **
+******************************************************************/
+
+#endregion // CLASS DEFINITION
+
+
+#region USING DIRECTIVES
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+#endregion // USING DIRECTIVES
+
+#region OBJECTSHREDDER CLASS DEFINITION
+public class ObjectShredder : MonoBehaviour
+{
+    #region UNITY FUNCTIONS
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(other.gameObject);
+    }
+
+    #endregion // UNITY FUNCTIONS
+
+} // Class ObjectShredder
+
+#endregion // CLASS DEFINITION
