82a9dc2b6ee48323f5de1f6da18c05a3bf414924
Josenn <josenn44@gmail.com>
2020-06-17 19:04:45 -0500

    Added Gravestone defender (or was this folder tidying?)

---
+++ Assets/Scripts/Defenders/Gravestone/Gravestone.cs
@@ -0,0 +1,81 @@
+﻿#region CLASS DESCRIPTION
+
+/******************************************************************
+**                                                               **
+**              Jonathan Renn                                    **
+**                                                               **
+**              Copyright (C) 2020 - All Rights Reserved         **
+**                                                               **
+*******************************************************************
+**                                                               **
+**      Project:             Glitch Garden                       **
+**                                                               **
+**      Module:              Defender                            **
+**                                                               **
+**      File name:           Gravestone.cs                       **
+**                                                               **
+**      Created by:          06/17/20            - JJR           **
+**                                                               **
+**      Description:         Controls certain properties of      **
+**                           the gravestone defender             **
+**                                                               **
+**                                                               **
+**                                                               **
+**                                                               **
+**                                                               **
+**                                                               **
+******************************************************************/
+
+#endregion // CLASS DEFINITION
+
+
+#region USING DIRECTIVES
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+#endregion // USING DIRECTIVES
+
+
+#region GRAVESTONE CLASS DEFINITION
+public class Gravestone : MonoBehaviour
+{
+    #region PUBLIC VARIABLES
+
+    #endregion // PUBLIC VARIABLES
+
+	#region PRIVATE VARIABLES
+
+
+    #endregion // PRIVATE VARIABLES
+
+    #region UNITY FUNCTIONS
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    #endregion // UNITY FUNCTIONS
+
+    #region PUBLIC FUNCTIONS
+
+    #endregion // PUBLIC FUNCTIONS
+
+	#region PRIVATE FUNCTIONS
+
+	#endregion // PRIVATE FUNCTIONS
+
+	#region TODOS
+
+	#endregion // TODOS
+
+} // Class Gravestone
+
+#endregion // CLASS DEFINITION
