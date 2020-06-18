f712a8b20589c783f9f0a248f9dd28f494072ccf
Josenn <josenn44@gmail.com>
2020-06-17 19:00:52 -0500

    Added a game timer

---
+++ Assets/Scripts/UI/Slider/GameTimer.cs
@@ -0,0 +1,86 @@
+﻿#region CLASS DESCRIPTION
+
+/******************************************************************
+**                                                               **
+**              Jonathan Renn                                    **
+**                                                               **
+**              Copyright (C) 2020 - All Rights Reserved         **
+**                                                               **
+*******************************************************************
+**                                                               **
+**      Project:             Glitch Garden                       **
+**                                                               **
+**      Module:              UI/Slider                           **
+**                                                               **
+**      File name:           GameTimer.cs                        **
+**                                                               **
+**      Created by:          06/17/20            - JJR           **
+**                                                               **
+**      Description:         Controls the in game timer          **
+**                                                               **
+**                                                               **
+**                                                               **
+**                                                               **
+**                                                               **
+**                                                               **
+**                                                               **
+******************************************************************/
+
+#endregion // CLASS DEFINITION
+
+
+#region USING DIRECTIVES
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+#endregion // USING DIRECTIVES
+
+
+#region GAMETIMER CLASS DEFINITION
+public class GameTimer : MonoBehaviour
+{
+    #region PUBLIC VARIABLES
+
+    #endregion // PUBLIC VARIABLES
+
+	#region PRIVATE VARIABLES
+
+    [Tooltip("Game timer in seconds")]
+    [SerializeField] private float gameTime = 10.0f;
+
+    #endregion // PRIVATE VARIABLES
+
+    #region UNITY FUNCTIONS
+
+    private void Update()
+    {
+        GetComponent<Slider>().value = Time.timeSinceLevelLoad / gameTime;
+
+        bool timerFinished = (Time.timeSinceLevelLoad >= gameTime);
+
+        if(timerFinished)
+        {
+            Debug.Log("Game timer expired!!");
+        }
+    }
+
+    #endregion // UNITY FUNCTIONS
+
+    #region PUBLIC FUNCTIONS
+
+    #endregion // PUBLIC FUNCTIONS
+
+	#region PRIVATE FUNCTIONS
+
+	#endregion // PRIVATE FUNCTIONS
+
+	#region TODOS
+
+	#endregion // TODOS
+
+} // Class GameTimer
+
+#endregion // CLASS DEFINITION
